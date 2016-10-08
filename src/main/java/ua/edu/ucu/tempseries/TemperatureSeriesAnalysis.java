f111e53b08cdfcbf2ac55ce44efe6eafce2f1788
kovtun_m <kovtun_m@ucu.edu.ua>
2016-10-08 12:32:38 +0300

    Added methods min and max. Added test for min and max methods, added yet empty tests for other methods. All tests are green.

--- src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
+++ src/main/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysis.java
@@ -1,6 +1,7 @@
 package ua.edu.ucu.tempseries;
 
 import java.util.Arrays;
+import java.util.InputMismatchException;
 
 public class TemperatureSeriesAnalysis {
     protected double[] temperatureSeries;
@@ -10,14 +11,19 @@ public class TemperatureSeriesAnalysis {
     }
 
     public TemperatureSeriesAnalysis(double[] temperatureSeries) {
-        if (temperatureSeries.length == 0 ) {
+        if (temperatureSeries.length == 0) {
             throw new IllegalArgumentException("List is empty");
+        }
+
+        for (double tmp : temperatureSeries) {
+            if (tmp < -273) {
+                throw new InputMismatchException();
+            }
         }
         this.temperatureSeries = temperatureSeries;
     }
 
     public double average() {
-
         double avg = 0;
         for (double tmp : temperatureSeries) {
             avg += tmp / temperatureSeries.length;
@@ -31,12 +37,25 @@ public class TemperatureSeriesAnalysis {
     }
 
     public double min() {
-        return 0;
+        double minimalValue = temperatureSeries[0];
+        for (double tmp : temperatureSeries) {
+            if (tmp < minimalValue) {
+                minimalValue = tmp;
+            }
+        }
+        return minimalValue;
     }
 
     public double max() {
-        return 0;
+        double maximumValue = temperatureSeries[0];
+        for (double tmp : temperatureSeries) {
+            if (tmp > maximumValue) {
+                maximumValue = tmp;
+            }
+        }
+        return maximumValue;
     }
+
 
     public double findTempClosestToZero() {
         return 0;
--- src/test/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysisTest.java
+++ src/test/java/ua/edu/ucu/tempseries/TemperatureSeriesAnalysisTest.java
@@ -1,11 +1,106 @@
 package ua.edu.ucu.tempseries;
 
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.InputMismatchException;
 
 public class TemperatureSeriesAnalysisTest {
+    @Test
+    public void deviation() throws Exception {
+
+    }
+
+    @Test
+    public void findTempClosestToZero() throws Exception {
+
+    }
+
+    @Test
+    public void findTempClosestToValue() throws Exception {
+
+    }
+
+    @Test
+    public void findTempsLessThen() throws Exception {
+
+    }
+
+    @Test
+    public void findTempsGreaterThen() throws Exception {
+
+    }
+
+    @Test
+    public void summaryStatistics() throws Exception {
+
+    }
+
+    @Test
+    public void addTemps() throws Exception {
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMaxWithEmptyArray() {
+        double[] temperatureSeries = {};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+
+        // expect exception here
+        seriesAnalysis.min();
+    }
+
+    @Test
+    public void testMaxWithArray() {
+        double[] temperatureSeries = {-3.0, -1.0, 2.0, 137.5, 228, 388, 843};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+        double expResult = 843;
+        double actualResult = seriesAnalysis.max();
+        assertEquals(expResult, actualResult, 0.00001);
+    }
+
+    @Test(expected = InputMismatchException.class)
+    public void testMinWithNegativeValue() {
+        double[] temperatureSeries = {-273.1};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+        seriesAnalysis.min();
+    }
+
+    @Test
+    public void testMinWithArray() {
+        double[] temperatureSeries = {-3.0, -1.0, 2.0, 137.5, 228, 388, 843};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+        double expResult = -3.0;
+        double actualResult = seriesAnalysis.min();
+        assertEquals(expResult, actualResult, 0.00001);
+    }
+
+    @Test
+    public void testMinWithOneElementArray() {
+        // setup input data and expected result
+        double[] temperatureSeries = {-1.0};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+        double expResult = -1.0;
+
+        // call tested method
+        double actualResult = seriesAnalysis.min();
+
+        // compare expected result with actual result
+        assertEquals(expResult, actualResult, 0.00001);
+
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMinWithEmptyArray() {
+        double[] temperatureSeries = {};
+        TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
+
+        // expect exception here
+        seriesAnalysis.min();
+    }
+
 
     @Test
     public void testAverageWithOneElementArray() {
@@ -33,14 +128,14 @@ public class TemperatureSeriesAnalysisTest {
     @Test
     public void testAverage() {
         double[] temperatureSeries = {3.0, -5.0, 1.0, 5.0};
-   //     System.out.println(Arrays.toString(temperatureSeries));
+        //     System.out.println(Arrays.toString(temperatureSeries));
         TemperatureSeriesAnalysis seriesAnalysis = new TemperatureSeriesAnalysis(temperatureSeries);
         double expResult = 1.0;
 
         double actualResult = seriesAnalysis.average();
-        
-        assertEquals(expResult, actualResult, 0.00001);        
+
+        assertEquals(expResult, actualResult, 0.00001);
     }
-    
+
 
 }
